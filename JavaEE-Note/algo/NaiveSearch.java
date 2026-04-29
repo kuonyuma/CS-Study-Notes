4f38b30cd3064d843ae2d45f27d01e0384b8f6aa
kuonyuma <236031735+kuonyuma@users.noreply.github.com>
2026-04-29 14:19:32 +0800

    优化后的暴力算法

---
+++ JavaEE-Note/algo/NaiveSearch.java
@@ -0,0 +1,25 @@
+
+public class MyFind  {
+
+    public int find(byte[] father,int faherLength,
+                    byte[]Sum,int SumLength){
+        byte first = Sum[0];
+        int max = faherLength - SumLength;
+        for(int i = 0; i <= max; i++) {
+            if (father[i] != first){
+                while (++i < max && father[i] != first) ;
+            }
+
+            if(i <= max){
+                int j = i + 1;
+                int end = j + SumLength - 1;
+                for(int k = 1;father[j] == Sum[k] && j <= end;j++,k++);
+
+                if(j == end){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
