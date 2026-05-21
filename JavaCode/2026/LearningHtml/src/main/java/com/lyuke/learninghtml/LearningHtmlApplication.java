3b2833e38dd9b83e423b79e901d69fa0c4eb8ade
kuonyuma <236031735+kuonyuma@users.noreply.github.com>
2026-05-21 16:04:26 +0800

    前端代码小练习

---
+++ JavaCode/2026/LearningHtml/src/main/java/com/lyuke/learninghtml/LearningHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.lyuke.learninghtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningHtmlApplication.class, args);
+    }
+
+}
---
+++ JavaCode/2026/LearningHtml/src/test/java/com/lyuke/learninghtml/LearningHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyuke.learninghtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
