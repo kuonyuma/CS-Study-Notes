ce439363f9e241542d1d5b1a7475f85db9b7bba5
kuonyuma <236031735+kuonyuma@users.noreply.github.com>
2026-06-11 09:21:48 +0800

    zu

---
+++ JavaCode/2026/demo/src/main/java/com/example/Print.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Print {
+    public void sout() {
+        System.out.println("nihao");
+    }
+
+    public static void main(String[] args) {
+        Print print = new Print();
+        print.sout();
+    }
+}
